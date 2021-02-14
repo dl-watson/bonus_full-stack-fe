16b9243cb4da1a01a1c15103b7f2dabf74678f1a
dl-watson <dlarkinwatson@gmail.com>
2021-02-13 21:26:10 -0800

    pass down props to Frogs; updating return method to clear console erros

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { useFrogs } from "../../services/useFrogs";
+import Frogs from "./frogs/Frogs";
+import useFrogs from "../../services/useFrogs";
 
 const App = () => {
   const { name, image, description, handleSubmit } = useFrogs();
 
-  return <div></div>;
+  return (
+    <div>
+      <Frogs
+        name={name}
+        image={image}
+        description={description}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/app/frogs/Frogs.jsx
+++ src/components/app/frogs/Frogs.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Frogs = () => {};
+const Frogs = () => {
+  return <>Frogs</>;
+};
 
 Frogs.propTypes = {};
 
